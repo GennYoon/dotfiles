7387c2e8deb161287efdd09c40ae8199568c200b
GennYoon <yoonwonyoul@webchemist.net>
2024-02-04 23:16:52 +0900

    modified some
    tmux와 Brewfile 수정, lazy에 언어 추가

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -89,7 +89,9 @@ return {
 
   {
     "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
     end,
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -56,7 +56,7 @@ return {
   {
     "rcarriga/nvim-notify",
     opts = {
-      timeout = 1000,
+      timeout = 2000,
     },
   },
 
@@ -96,42 +96,43 @@ return {
     opts = {
       options = {
         -- globalstatus = false,
-        theme = "solarized_dark",
+        -- theme = "solarized_dark",
+        theme = "onedark",
       },
     },
   },
 
   -- filename
-  {
-    "b0o/incline.nvim",
-    dependencies = { "craftzdog/solarized-osaka.nvim" },
-    event = "BufReadPre",
-    priority = 1200,
-    config = function()
-      local colors = require("solarized-osaka.colors").setup()
-      require("incline").setup({
-        highlight = {
-          groups = {
-            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-          },
-        },
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "b0o/incline.nvim",
+  --   dependencies = { "craftzdog/solarized-osaka.nvim" },
+  --   event = "BufReadPre",
+  --   priority = 1200,
+  --   config = function()
+  --     local colors = require("solarized-osaka.colors").setup()
+  --     require("incline").setup({
+  --       highlight = {
+  --         groups = {
+  --           InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+  --           InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+  --         },
+  --       },
+  --       window = { margin = { vertical = 0, horizontal = 1 } },
+  --       hide = {
+  --         cursorline = true,
+  --       },
+  --       render = function(props)
+  --         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+  --         if vim.bo[props.buf].modified then
+  --           filename = "[+] " .. filename
+  --         end
+  --
+  --         local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+  --         return { { icon, guifg = color }, { " " }, { filename } }
+  --       end,
+  --     })
+  --   end,
+  -- },
 
   {
     "folke/zen-mode.nvim",
@@ -162,5 +163,6 @@ return {
       logo = string.rep("\n", 8) .. logo .. "\n\n"
       opts.config.header = vim.split(logo, "\n")
     end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
   },
 }
