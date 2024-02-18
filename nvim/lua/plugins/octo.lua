74df615a4012745a69483f276627ef1f62ab6735
GennYoon <yoonwonyoul@webchemist.net>
2024-02-18 11:58:34 +0900

    modified tmux bind key & changed alacrltty binding key & add octo in whichkey

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -7,7 +7,7 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("octo").setup({ enable_builtin = true })
+    require("octo").setup({ enable_builtin = true, suppress_missing_scope = { projects_v2 = true } })
     vim.cmd([[hi OctoEditable guibg=none]])
   end,
 }
