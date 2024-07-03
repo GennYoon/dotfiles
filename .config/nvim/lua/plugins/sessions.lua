72d97f2f4d769ca9784d7968af3178d4561ea3fd
GennYoon <yoonwonyoul@webchemist.net>
2024-07-03 09:32:57 +0900

    chore: update dotfiles

--- .config/nvim/lua/plugins/sessions.lua
+++ .config/nvim/lua/plugins/sessions.lua
@@ -1,15 +1,16 @@
 return {
   "rmagatti/auto-session",
   config = function()
-    require("auto-session").setup({
-      auto_session_suppress_dirs = { "~/", "~/Workspace", "~/Downloads", "/" },
-      session_lens = {
-        buftypes_to_ignore = {},
-        load_on_setup = true,
-        theme_conf = { border = true },
-        previewer = false,
-      },
-      -- vim.keymap.set("n", "<Leader>ls", require("auto-session.session-lens").search_session, { noremap = true, }),
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
     })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
   end,
 }
