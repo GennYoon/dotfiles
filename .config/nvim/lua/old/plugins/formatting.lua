12008fbdf1f8105f24ec7a1ac111f5a01ee5486c
GennYoon <yoonwonyoul@webchemist.net>
2024-08-24 23:05:52 +0900

    update formatting

--- .config/nvim/lua/old/plugins/formatting.lua
+++ .config/nvim/lua/old/plugins/formatting.lua
@@ -1,35 +1,34 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  event = { "LspAttach", "BufReadPre", "BufNewFile" },
   config = function()
     local conform = require("conform")
 
     conform.setup({
       formatters_by_ft = {
-
         lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        svelte = { { "prettierd", "prettier" } },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
         dart = { "dart_format" },
         rust = { "rustfmt" },
         java = { "google-java-format" },
         kotlin = { "ktlint" },
         ruby = { "standardrb" },
-        css = { { "prettierd", "prettier" } },
-        scss = { { "prettierd", "prettier" } },
+        css = { "prettier" },
+        scss = { "prettier" },
         html = { "htmlbeautifier" },
-        json = { { "prettierd", "prettier" } },
+        json = { "prettier" },
         yaml = { "yamlfix" },
         toml = { "taplo" },
-        markdown = { { "prettierd", "prettier" } },
-        graphql = { { "prettierd", "prettier" } },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
         python = { "isort", "black" },
       },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 1000,
         lsp_format = "fallback",
         async = false,
       },
