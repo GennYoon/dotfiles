065b4e93e73df2ce966713456fa3af76711eff36
GennYoon <yoonwonyoul@webchemist.net>
2025-03-16 23:38:24 +0900

    faet: denols & vtsls lsp 수정

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local nvim_lsp = require("lspconfig")
+
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
@@ -6,6 +8,37 @@ return {
       cssls = {
         -- NOTE: tailwindcss intellisense
         settings = { css = { lint = { unknownAtRules = "ignore" } } },
+      },
+      denols = {
+        filetypes = { "typescript", "typescriptreact" },
+        root_dir = function(...)
+          return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+        end,
+        handlers = {
+          ["textDocument/publishDiagnostics"] = nvim_lsp.diagnostic.on_publish_diagnostics,
+        },
+        settings = {
+          deno = {
+            enable = true,
+            lint = true,
+            unstable = true,
+            suggest = {
+              imports = {
+                hosts = {
+                  ["https://deno.land"] = true,
+                  ["https://cdn.nest.land"] = true,
+                  ["https://crux.land"] = true,
+                },
+              },
+            },
+          },
+        },
+      },
+      vtsls = {
+        root_dir = function(filename)
+          return nvim_ls.util.root_pattern("package.json")(filename)
+        end,
+        single_file_support = false,
       },
     },
   },
