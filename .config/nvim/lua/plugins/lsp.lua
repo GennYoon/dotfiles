1061ab42ebdb8740781ab2eb9cf1c39045acca49
GennYoon <yoonwonyoul@webchemist.net>
2025-03-17 19:34:33 +0900

    feat: diagnostic

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -42,4 +42,18 @@ return {
       },
     },
   },
+  config = function()
+    -- Change the Diagnostic symbols in the sign column (gutter)
+    -- (not in youtube nvim video)
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
+    vim.diagnostic.config({
+      virtual_text = true,
+      signs = false,
+    })
+  end,
 }
