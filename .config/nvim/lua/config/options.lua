0579ce6cd710c91a824de73f00f23e6d70bd8d3a
GennYoon <yoonwonyoul@webchemist.net>
2025-05-30 17:56:23 +0900

    feat: update sql

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 vim.g.mapleader = " "
 -- Enable this option to avoid conflicts wdith Prettier.
 vim.g.lazyvim_prettier_needs_config = true
